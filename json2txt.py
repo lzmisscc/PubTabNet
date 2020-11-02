3ccfad6c7eaa63c1f67f028b3cd9601e41b816e4
lzmisscc <1028741371@qq.com>
2020-11-02 11:49:17 +0800

    创建数据集

--- json2txt.py
+++ json2txt.py
@@ -1,32 +1,60 @@
 from __future__ import barry_as_FLUFL
-import PIL
-from shapely.geometry import polygon
 import jsonlines
 import logging
-from PIL import Image, ImageDraw, ImagePath
 import os
-import numpy as np
+from PIL import Image
 
 logging.basicConfig(
     level=logging.INFO
 )
 
-reader = jsonlines.open("", "r")
+reader = jsonlines.open(
+    "/data/ouyangshizhuang/data/table_match/pubtabnet/PubTabNet_2.0.0.jsonl", "r")
+
+reader = iter(reader)
 
 
-def get_text(img: dict) -> list:
+def get_text(img: dict, dataset_path: str = "/data/ouyangshizhuang/data/table_match/pubtabnet/") -> list:
+    flag = img['split']
+    image_path = os.path.join(dataset_path, flag, img['filename'])
+    image = Image.open(image_path)
     text_list = []
+    points = []
+    corp_imgs = []
     for cell in img['html']['cells']:
-        if 'tokens' not in cell:
+        if 'bbox' not in cell:
             continue
-        text_list.append(''.join(cell['tokens']+['\n']))
-        
-    if img['split'] == 'train':
-        with open(os.path.join("train", img['filename']), 'w') as f:
-            f.writelines(text_list)
-    elif img['split'] == 'val':
-        with open(os.path.join("val", img['filename']), 'w') as f:
-            f.writelines(text_list)
-    elif img['split'] == 'test':
-        with open(os.path.join("test", img['filename']), 'w') as f:
-            f.writelines(text_list)
+        text_list.append(''.join(cell['tokens']))
+        points.append(cell['bbox'])
+        corp_imgs.append(image.crop(cell['bbox']))
+
+    return {
+        'text_list': text_list,
+        'corp_imgs': corp_imgs,
+        'points': points,
+        'filename': img['filename'],
+        'flag': img['split']
+    }
+
+
+def main():
+    data = next(reader)
+    result = get_text(data)
+    logging.info(result['filename'])
+    index = 0
+    for text, crop_img in zip(result['text_list'], result['corp_imgs']):
+        index += 1
+        pre = str(index).zfill(5)
+        crop_save_name = result['filename'].replace('.png', f'_{pre}.png')
+        crop_txt_save_name = crop_save_name.replace('.png', '.txt')
+        logging.info(crop_txt_save_name)
+        with open(os.path.join('dataset', result['flag'], crop_txt_save_name), 'w') as f:
+            f.write(text)
+        crop_img.save(os.path.join(
+            'dataset', result['flag'], crop_save_name))
+    return
+
+
+if __name__ == "__main__":
+    for i in range(5000):
+        main()
