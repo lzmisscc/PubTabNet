7ffd3e1d297af511cfba366ee4d3f981f2423aea
lzmisscc <1028741371@qq.com>
2020-11-06 09:28:48 +0800

    表格读取方法

--- table2row_col.py
+++ table2row_col.py
@@ -6,28 +6,62 @@ import logging
 from PIL import Image, ImageDraw, ImagePath
 import os
 import numpy as np
+import re
 
 logging.basicConfig(
     level=logging.INFO
 )
-
-
-def read_json_lines():
-    with jsonlines.open('examples/PubTabNet_Examples.jsonl', 'r') as reader:
-        imgs = list(reader)
-    return imgs
+reader = jsonlines.open('examples/PubTabNet_Examples.jsonl', 'r').iter
 
 
 def polygon(gt: dict) -> PIL.Image:
     img_path = os.path.join("examples/", gt['filename'])
     pil_img = Image.open(img_path)
-    draw = ImageDraw.Draw(pil_img)
-    W, H = pil_img.size
-    cells = gt['cells']
-    bboxs = []
+    draw = ImageDraw.Draw(pil_img, mode='RGBA')
+    # 找出表格的宽和高
+    cells = gt['html']['cells']
+    bboxes = []
     for cell in cells:
-        if not cell['bbox']:
+        if 'bbox' not in cell:
             continue
-        bboxs.append(cell['bbox'])
+        bboxes.append(cell['bbox'])
+    bboxes = np.array(bboxes)
+    left, top, right, buttom = np.min(bboxes[:, 0]), np.min(
+        bboxes[:, 1]), np.max(bboxes[:, 2]), np.max(bboxes[:, 3])
+
+    draw.rectangle([left, top, right, buttom], fill=(255, 0, 0, 150))
+    W, H = right-left, buttom-top
+
+    # 找出每行和列的关系
+    rows, cell_nums = 0, 0
+    table = []
+    structure = gt['html']['structure']['tokens']
+    structure_len = len(structure)
+    for index, cell in enumerate(structure):
+        if '<tr' in cell:
+            table.append([])
+        elif '<td' in cell:
+            if index < structure_len and 'colspan' in structure[index+1]:
+                # 计算出列的数量
+                nums = re.search(r'[0-9]+', structure[index+1])
+                if nums:
+                    nums = int(nums.group())
+                    table[-1].append([[nums] for _ in range(nums)])
+                continue
+            elif  index < structure_len and 'rowspan' in structure[index+1]:
+                # 计算出列的数量
+                nums = re.search(r'[0-9]+', structure[index+1])
+                if nums:
+                    nums = int(nums.group())
+                    table[-1].append([-nums])
+                continue
+            table[-1].append([1])
+    print(*table, sep='\n')
+    pil_img.save(f"vis_col_row/{gt['filename']}")
+
+    return pil_img
 
 
+for index, line in enumerate(reader()):
+    logging.info(f"{index}->{line['filename']}")
+    polygon(line)
---
+++ table2row_col_test.py
@@ -0,0 +1,66 @@
+from __future__ import barry_as_FLUFL
+import PIL
+from shapely.geometry import polygon
+import jsonlines
+import logging
+from PIL import Image, ImageDraw, ImagePath
+import os
+import numpy as np
+import re
+
+logging.basicConfig(
+    level=logging.INFO
+)
+reader = jsonlines.open('examples/PubTabNet_Examples.jsonl', 'r').iter
+
+
+def get_bbox(points: list) -> list:
+    # [[], ...,[]] -> [x_min,y_min,x_max,y_max]
+    points = np.array(points)
+    return list(map(int, [
+        np.min(points[:, 0]),
+        np.min(points[:, 1]),
+        np.max(points[:, 2]),
+        np.max(points[:, 3]),
+    ]))
+
+
+def polygon(gt: dict) -> PIL.Image:
+    img_path = os.path.join("examples/", gt['filename'])
+    pil_img = Image.open(img_path)
+    draw = ImageDraw.Draw(pil_img, mode='RGBA')
+
+    html = ''.join(gt['html']['structure']['tokens'])
+
+    thead = re.findall(r'<thead>(.*?)</thead>', html)
+    thead_nums = len(re.findall(r'<td.*?</td>', thead[0]))
+
+    tr = re.findall(r'<tr>(.*?)</tr>', html)
+
+    td = re.findall(r'<td.*?</td>', html)
+
+    tbody = re.findall(r'<tbody>(.*?)</tbody>', html)
+    tbody_nums = len(re.findall(r'<td.*?</td>', tbody[0]))
+
+    cells = gt['html']['cells']
+
+    table_bbox = get_bbox([i['bbox']
+                        for i in cells if 'bbox' in i])
+    tabele_W, table_H = table_bbox[2] - \
+        table_bbox[0], table_bbox[3]-table_bbox[1]
+    # thead
+    thead_bbox = get_bbox([i['bbox']
+                           for i in cells[0:thead_nums] if 'bbox' in i])
+    thead_bbox = []
+    draw.rectangle(thead_bbox, fill=(255, 0, 0, 150))
+    tbody_bbox = get_bbox(
+        [i['bbox'] for i in cells[thead_nums:len(td)] if 'bbox' in i])
+    draw.rectangle(tbody_bbox, fill=(0, 0, 255, 150))
+
+    pil_img.save(f"vis_col_row/{gt['filename']}")
+    return pil_img
+
+
+for index, line in enumerate(reader()):
+    logging.info(f"{index}\t->{line['filename']}")
+    polygon(line)
