ee901f593e9b0ce079624db58bd61663303d93d5
lzmisscc <1028741317@qq.com>
2020-11-09 15:04:35 +0800

    完成行和列的处理

--- table2col_row.py
+++ table2col_row.py
@@ -28,11 +28,19 @@ def get_bbox(points: list) -> list:
     ]))
 
 
+def A_B(A, B):
+    x1, y1 = A[0], A[2]
+    x2, y2 = B[0], B[2]
+    if x1 > y2 or x2 > y1:
+        return False
+    else:
+        return min(x1, x2), 0, max(y1, y2), 0
+
+
 def polygon(gt: dict) -> PIL.Image:
     img_path = os.path.join("examples/", gt['filename'])
-    pil_img = Image.open(img_path)
-    draw = ImageDraw.Draw(pil_img, mode='RGBA')
-
+    pil_img_row = Image.open(img_path)
+    pil_img_col = pil_img_row.copy()
     html = ''.join(gt['html']['structure']['tokens'])
 
     thead = re.findall(r'<thead>(.*?)</thead>', html)
@@ -52,44 +60,43 @@ def polygon(gt: dict) -> PIL.Image:
     table_W, table_H = [table_bbox[0],
                         table_bbox[2]], [table_bbox[1], table_bbox[3]]
     # tr 统计td的数量进行bbox的对齐。
+    draw = ImageDraw.Draw(pil_img_row, mode='RGBA')
+
     td_nums = 0
     for tr_line in tr:
         num = len(re.findall(r'<td.*?</td>', tr_line))
         bbox = get_bbox([i['bbox']
                          for i in cells[td_nums:td_nums+num] if 'bbox' in i])
         bbox = [table_W[0], bbox[1], table_W[1], bbox[3]]
-        # draw.rectangle(bbox, fill=(0, 255, 0, 150), outline=(255, 0, 255))
+        draw.rectangle(bbox, fill=(0, 255, 0, 150), outline=(255, 0, 255))
         td_nums += num
+    pil_img_row.save(f"vis_row/{gt['filename']}")
 
     # td 进行投影决定列的宽度。
+    draw = ImageDraw.Draw(pil_img_col, mode='RGBA')
+
     td_nums = 0
-    cols = {}
+    bboxes = []
 
-    for tr_line in tr:
-        num = len(re.findall(r'<td.*?</td>', tr_line))
-        index = 0
-        for label, bbox in zip(re.findall(r'<td.*?</td>', tr_line), [i for i in cells[td_nums:td_nums+num]]):
-            if index not in cols:
-                cols[index] = []
-            x = re.findall(r'colspan="([0-9]+?)"', label)
-            y = re.findall(r'rowspan="([0-9]+?)"', label)
-            if y:
-                logging.info(f'y:{y}')
-            else:
-                y = 0 
-            if not x:
-                if 'bbox' in bbox:
-                    cols[index].append(bbox['bbox'])
-                index += 1
-            else:
-                index += sum(map(int, x))
+    for label, bbox in zip(td, cells):
+        if 'bbox' in bbox and not re.findall(r'colspan="([0-9]+?)"', label):
+            bboxes.append(bbox['bbox'])
 
-        td_nums += num
-    for key,values in cols.items():
-        values = np.array(values)
-        draw.rectangle([int(np.min(values[:, 0])), table_H[0], int(np.max(values[:, 2])), table_H[1]], fill=(255, 255, 0, 200), outline=(0, 0, 255))
-    pil_img.save(f"vis_col_row/{gt['filename']}")
-    return pil_img
+    cols = [bboxes.pop()]
+    while bboxes:
+        bbox = bboxes.pop()
+        flag = False
+        for index, val in enumerate(cols):
+            res = A_B(bbox, val)
+            if res:
+                flag = True
+                cols[index] = res
+        if not flag:
+            cols.append(bbox)
+    for bbox in cols:
+        draw.rectangle([bbox[0], table_H[0], bbox[2], table_H[1]], fill=(0, 255, 255, 150), outline=(0, 0, 255))
+
+    pil_img_col.save(f"vis_col/{gt['filename']}")
 
 
 for index, line in enumerate(reader()):
