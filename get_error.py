cf93dea6f0c4332268ed976af078f38838052963
lzmisscc <1028741317@qq.com>
2020-11-11 17:19:14 +0800

    .

--- get_error.py
+++ get_error.py
@@ -1,10 +1,12 @@
+from logging import log
+from PIL.ImageDraw import ImageDraw
 import jsonlines
 import os
 from PIL import Image
 import re
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='find.log')
+logging.basicConfig(level=logging.INFO, )
 
 
 class Get_Error:
@@ -27,15 +29,61 @@ class Get_Error:
                 if 'bbox' in cell:
                     bboxes.append(cell['bbox'])
             if bboxes == []:
-                img = Image.open(os.path.join(self.data_path, json['filename']))
+                img = Image.open(os.path.join(
+                    self.data_path, json['filename']))
                 img.save(f'error_image/{json["filename"]}')
                 logging.info(os.path.join(self.data_path, json['filename']))
 
         return
 
     def get_across_col_row(self, ):
+        from table2col_row import polygon
+        for json in self.read.iter():
+            row = [r['bbox']
+                   for r in polygon(json) if r['category_id'] == 0]
+            cells = json['html']['cells']
+            bboxes = [i['bbox']
+                      for i in cells if 'bbox' in i]
+            for i in bboxes:
+                num = 0
+                for j in row:
+                    if IOU(i, j):
+                        num += 1
+                    else:
+                        continue
+
+                    if num > 3:
+                        img = Image.open(os.path.join(
+                            self.data_path, json['filename']))
+                        draw = ImageDraw(img, mode='RGBA')
+
+                        for x in row:
+                            draw.rectangle(x, outline='blue')
+                        for y in bboxes:
+                            draw.rectangle(y, fill=(0, 255, 0, 150))
+                        draw.rectangle(i, fill=(255, 255, 0, 155))
+
+                        img.save(f'error_across_image/{json["filename"]}')
+                        logging.info(f"num:\t{num}")
+                        logging.info(os.path.join(
+                            self.data_path, json['filename']))
+                        break
+                if num > 3:
+                    break
         return
 
 
+def IOU(A, B) -> bool:
+    if min(A[2], B[2]) - max(A[0], B[0]) > 0 and min(A[3], B[3]) - max(A[1], B[1]) > 0:
+        if (min(A[2], B[2]) - max(A[0], B[0])) * (min(A[3], B[3]) - max(A[1], B[1])) > 3500:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
-    Get_Error('train').get_none_head()
+    # Get_Error('train').get_none_head()
+    Get_Error('train').get_across_col_row()
+    # logging.info(IOU([1, 1, 1.5, 1.5], [1, 1, 1, 1], ))
