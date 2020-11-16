bf4563cb0f6838141023ad5ae1fcc469414080e7
lzmisscc <1028741317@qq.com>
2020-11-16 19:05:11 +0800

    使用多线程

--- table2ocr_dataset.py
+++ table2ocr_dataset.py
@@ -6,11 +6,11 @@ import numpy as np
 from find_text_area import detect
 
 logging.basicConfig(level=logging.INFO, )
-data_path = "/data/liuzhuang/DataSet/pubtabnet/"
-save_data_path = "table_ocr_dataset"
+data_path = "/data/liuzhuang/DataSet/pubtabnet/" # 图片路径
+save_data_path = "table_ocr_dataset"             # 保存路径 //train //val
 reader = jsonlines.open(
     "/data/liuzhuang/DataSet/pubtabnet/PubTabNet_2.0.0.jsonl").iter()
-
+# jsonl 路径
 
 def multi__lines_image_text(image: Image, filename: str):
     source = image.copy()
@@ -39,40 +39,63 @@ def multi__lines_image_text(image: Image, filename: str):
 
 
 logging.info('START'.center(50, '-'))
+class Gen_Txt:
+    def __init__(self) -> None:
+        self.train_txt, self.val_txt = [], []
 
-train_txt, val_txt = [], []
-for id, json in enumerate(reader):
-    text = json['html']['cells']
-    try:
-        img = Image.open(os.path.join(
-            data_path, json['split'], json['filename']))
-    except:
-        img = Image.open(os.path.join(data_path,  json['filename']))
-    finally:
-        logging.info(f"{id}\t{json['filename']}")
 
-        for index, _ in enumerate(text):
-            if 'bbox' not in _:
-                continue
-            image, text = img.crop(_['bbox']), ''.join(_['tokens'])
+    def func(self, reader):
+        for id, json in enumerate(reader):
+            text = json['html']['cells']
+            try:
+                img = Image.open(os.path.join(
+                    data_path, json['split'], json['filename']))
+            except:
+                img = Image.open(os.path.join(data_path,  json['filename']))
+            finally:
+                # logging.info(f"{id}\t{json['filename']}")
 
-            save_name = json['filename'][:-4] + \
-                '_' + str(index).zfill(5) + '.png'
-            image = multi__lines_image_text(image, save_name+'\t'+text)
-            image.save(os.path.join(save_data_path, json['split'], save_name))
-            if json['split'] == 'train':
-                train_txt.append(f'{save_name}\t{text}\n')
-            elif json['split'] == 'val':
-                val_txt.append(f'{save_name}\t{text}\n')
-            else:
-                logging.info(f"ERROR SPLIT {json['split']}")
+                for index, _ in enumerate(text):
+                    if 'bbox' not in _:
+                        continue
+                    image, text = img.crop(_['bbox']), ''.join(_['tokens'])
 
-logging.info("Save_Train".center(50, '-'))
-with open(os.path.join(save_data_path, 'train.txt'), 'w') as f:
-    f.writelines(train_txt)
+                    save_name = json['filename'][:-4] + \
+                        '_' + str(index).zfill(5) + '.png'
+                    image = multi__lines_image_text(image, save_name+'\t'+text)
+                    image.save(os.path.join(save_data_path, json['split'], save_name))
+                    if json['split'] == 'train':
+                        self.train_txt.append(f'{save_name}\t{text}\n')
+                    elif json['split'] == 'val':
+                        self.val_txt.append(f'{save_name}\t{text}\n')
+                    else:
+                        logging.info(f"ERROR SPLIT {json['split']}")
+    def save(self, ):
+        from multiprocessing import Process
+        read = list(reader)
+        logging.info(f"Reading, {len(read)}条")
+        poolings = []
+        i = 0
+        for i in range(0, len(read)//100000):
+            p = Process(target=self.func, args=(read[i*100000:(i+1)*100000], ))
+            p.start()
+            poolings.append(p)
+        p = Process(target=self.func, args=(read[(i+1)*100000:], ))
+        p.start()
+        poolings.append(p)
+        for i in poolings:
+            i.join()
 
-logging.info("Save_Test".center(50, '-'))
-with open(os.path.join(save_data_path, 'val.txt'), 'w') as f:
-    f.writelines(val_txt)
 
-logging.info("END".center(50, '-'))
+        logging.info("Save_Train".center(50, '-'))
+        with open(os.path.join(save_data_path, 'train.txt'), 'w') as f:
+            f.writelines(self.train_txt)
+
+        logging.info("Save_Test".center(50, '-'))
+        with open(os.path.join(save_data_path, 'val.txt'), 'w') as f:
+            f.writelines(self.val_txt)
+
+        logging.info("END".center(50, '-'))
+
+if __name__ == "__main__":
+    Gen_Txt().save()
